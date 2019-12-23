e60c6edae613695c485bad5dd31e724af0a0297e
MrFoxington <j.arcoudis@gmail.com>
2019-12-24 00:37:44 +1100

    OH GOD WHY! Attempt to build smart solve funtionality. (This will not compile, swapping branches for work)

--- solver.py
+++ solver.py
@@ -1,7 +1,10 @@
-from copy import copy
-
+import copy
 import sudoku
 import boards
+
+
+# TODO: Move all guesses logic into Sudoku class.....
+# This is dumb the amount of janky solutions so similar to the puzzle board logic
 
 
 class Solver:
@@ -36,6 +39,12 @@ class Solver:
         guesses = [x for x in guesses if x not in puzzle.get_box(row, col)]
         return guesses
 
+    # Oh god, send help!. This is turning into a clusterfuck...
+    def update_guesses_by_cell(self, guesses, puzzle, row, col):
+         # Get all other cells affected by updated Cell
+         # Itterate through cells and remove value from guesses from
+        pass
+
     # TODO: Learn Backtracking algorithm
     def solve(self):
         empty = 0
@@ -45,10 +54,46 @@ class Solver:
         self.__solve_dumb(self.puzzle, empty)
 
     # Smart Recursive Solver (locates lowest guess count)
-    def __solve(self, puzzle, guesses):
-        pass
+    def __solve(self, puzzle, guesses, empty):
+        print('Solving!')
+        if empty <= 0:
+            if puzzle.validate_board():
+                print(puzzle)
+                return True
+            return False
+
+        # Get Cell to work on (check guesses for cell with least possibilities)
+        row, col = self.__find_best_cell_row_col(guesses)
+        possibilities = guesses[row][col]
+        tmp_puzzle = copy.deepcopy(puzzle)
+        for val in possibilities:
+            tmp_puzzle.board[row][col] = val
+            # DISGUSTING! Hate having to copy guesses every loop...
+            # Data is lost when updating guesses -> Need to re-copy original guesses each try
+            tmp_guesses = copy.deepcopy(self.update_guesses_by_cell(guesses, tmp_puzzle, row, col))
+            tmp_guesses[row][col] = []
+            if self.__solve(tmp_puzzle, tmp_guesses, empty - 1):
+                puzzle = tmp_puzzle
+                guesses = tmp_guesses
+                return True
+            else:
+                tmp_puzzle.board[row][col] = 0
+        return False
+
+    # Find cell with least options (Guess count)
+    def __find_best_cell_row_col(self, guesses):
+        # [Guess Count, row, col)
+        best_cell = None
+        for x, row in enumerate(guesses):
+            for y, cell in enumerate(row):
+                if len(cell) > 0:
+                    if best_cell is None: best_cell = [len(cell), x, y]
+                    if len(cell) < best_cell[0]: best_cell = [len(cell), x, y]
+                    if len(cell) == 1: return x, y
+        return best_cell[1], best_cell[2]
 
     # Dumb Recursive Solver
+    # Unable to tell if valid, CBF letting it run for long enough to see if it finds a valid solution
     def __solve_dumb(self, puzzle, empty):
         print('Solving!')
         if empty <= 0:
@@ -61,10 +106,10 @@ class Solver:
             for y, cell in enumerate(row):
                 if cell == 0:
                     guesses = self.get_cell_guesses(puzzle, x, y)
-                    puz = copy(puzzle) # Puzzle and Puz are still pointing to the same object,
+                    puz = copy.deepcopy(puzzle)  # Puzzle and Puz are still pointing to the same object,
                     for val in guesses:
                         puz.board[x][y] = val
-                        if self.__solve_dumb(puz, empty-1):
+                        if self.__solve_dumb(puz, empty - 1):
                             puzzle = puz
                             return True
                         else:
@@ -76,5 +121,3 @@ s = Solver()
 s.load_puzzle(boards.board_easy)
 s.solve()
 print("Done Run!")
-
-
