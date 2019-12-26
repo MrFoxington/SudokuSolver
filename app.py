6f3b476f65e79298e7e6b7931c5e8f9057b10e1e
John Fox <J.Arcoudis@gmail.com>
2019-12-26 14:52:23 +1100

    Update app.py
    Solver now designed to initiate from app.py
    Remove test code.

--- app.py
+++ app.py
@@ -1,16 +1,8 @@
 import boards
 import solver
 
-# print('testing')
-# s = solver.Solver()
-# s.load_puzzle(boards.board_easy)
-# print('done')
-
-
-def sayHi(count):
-    print('HI CUTIE!!!')
-    if count >= 0:
-        sayHi(count-1)
-
-
-sayHi(10)
+print('testing')
+s = solver.Solver()
+s.load_puzzle(boards.board_evil2)
+s.solve()
+print('Done')
